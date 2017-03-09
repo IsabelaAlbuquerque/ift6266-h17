52c5bceaa0cfe4bb1dff1a469038307874fa4814
Isabela Albuquerque <isabelamcalbuquerque@gmail.com>
2017-03-09 14:44:16 -0500

    Current version

--- Project/cnnL2AE.py
+++ Project/cnnL2AE.py
@@ -6,22 +6,17 @@ from keras.layers import Dense, Dropout, Activation, Flatten, Reshape, Input
 from keras.layers import Convolution2D, MaxPooling2D, Deconvolution2D, UpSampling2D
 from keras.utils import np_utils
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from build_minibatch import build_minibatch
 import os.path
+from data_flow import batch_generator
 
-batch_size = 128
-nb_outputs = 32*32*3
-nb_epoch = 10
+batch_size = 100
+nb_epoch = 50
+
+samples_per_epoch = 82611
+nb_val_samples = 40438
 
 # input image dimensions
 img_rows, img_cols = 64, 64
-
-# Preparing the training data
-inputs, targets, captions = build_minibatch(0, 128, 1, False)
-
-# Preparing the training data
-inputs_valid, targets_valid, captions_valid = build_minibatch(0, 128, 0, False)
-
 
 # number of convolutional filters to use
 nb_filters1 = 32
@@ -33,7 +28,7 @@ nb_filters6 = 64
 
 # number of deconvolutional filters to use
 nb_dfilters1 = 32
-nb_dfilters2 = 32
+nb_dfilters2 = 16
 nb_dfilters3 = 3
 
 # convolution kernel size
@@ -45,14 +40,14 @@ nb_conv5 = 4
 nb_conv6 = 4
 
 # deconvolution kernel size
-nb_dconv1 = 5
-nb_dconv2 = 5
+nb_dconv1 = 3
+nb_dconv2 = 3
 nb_dconv3 = 5
 
 # Stride
 str_conv1 = 3
 
-runMax = 50
+runMax = 300
 epoch=0
 run=0
 CPName = None
@@ -73,25 +68,24 @@ for i in range(runMax,-1,-1):
 
 if CPName is None:
 
-	input_img = Input(shape=(64,64,3))
+	input_img = Input(shape=(img_rows,img_cols,3))
 
-	x = Convolution2D(nb_filters1, nb_conv1, nb_conv1, activation='relu', border_mode='same', subsample=(str_conv1, str_conv1) )(input_img)
-	x = Convolution2D(nb_filters2, nb_conv2, nb_conv2, activation='relu', border_mode='same')(x)
-	x = Convolution2D(nb_filters3, nb_conv3, nb_conv3, activation='relu', border_mode='same')(x)
-	x = Convolution2D(nb_filters4, nb_conv4, nb_conv4, activation='relu', border_mode='same')(x)
-	x = Convolution2D(nb_filters5, nb_conv5, nb_conv5, activation='relu', border_mode='same')(x)
-	x = Convolution2D(nb_filters6, nb_conv6, nb_conv6, activation='relu', border_mode='same')(x)
+	x = Convolution2D(nb_filters1, nb_conv1, nb_conv1, activation='relu', border_mode='valid', subsample=(str_conv1, str_conv1) )(input_img)
+	x = Convolution2D(nb_filters2, nb_conv2, nb_conv2, activation='relu', border_mode='valid')(x)
+	x = Convolution2D(nb_filters3, nb_conv3, nb_conv3, activation='relu', border_mode='valid')(x)
+	x = Convolution2D(nb_filters4, nb_conv4, nb_conv4, activation='relu', border_mode='valid')(x)
+	x = Convolution2D(nb_filters5, nb_conv5, nb_conv5, activation='relu', border_mode='valid')(x)
+	x = Convolution2D(nb_filters6, nb_conv6, nb_conv6, activation='relu', border_mode='valid')(x)
 	x = Flatten()(x)
-	encoded = Dense(2000, activation='relu')(x)
+	encoded = Dense(2304, activation='relu')(x)
 	
-	x=Reshape([80,5,5])(encoded)
-	x = Convolution2D(nb_dfilters1, nb_dconv1, nb_dconv1, activation='relu', border_mode='same')(x)
+	x=Reshape([6,6,64])(encoded)
 	x = UpSampling2D((2,2) )(x)
-	x = Convolution2D(nb_dfilters2, nb_dconv2, nb_dconv2, activation='relu', border_mode='same')(x)
+	x = Convolution2D(nb_dfilters1, nb_dconv1, nb_dconv1, activation='relu', border_mode='valid')(x)
 	x = UpSampling2D((2,2) )(x)
-	x = Convolution2D(nb_dfilters3, nb_dconv3, nb_dconv3, activation='relu', border_mode='same')(x)
-	decoded = UpSampling2D((2,2) )(x)
-
+	x = Convolution2D(nb_dfilters2, nb_dconv2, nb_dconv2, activation='relu', border_mode='valid')(x)
+	x = UpSampling2D((2,2) )(x)
+	decoded = Convolution2D(nb_dfilters3, nb_dconv3, nb_dconv3, activation='sigmoid', border_mode='valid')(x)
 	autoencoder = Model(input_img, decoded)
 	autoencoder.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
 
@@ -101,15 +95,12 @@ else:
 	nb_epoch-=epoch
 	nb_epoch=max(nb_epoch,1)
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=35)
+early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 checkPointer = ModelCheckpoint(filepath='CP/cnnL2AE/cnnL2AE-'+str(run)+'-{epoch:02d}.h5', verbose=1, save_best_only=False, save_weights_only=False)
 
-history=autoencoder.fit(inputs, targets, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(inputs_valid, targets_valid), callbacks=[early_stopping, checkPointer])
+generatorTrain = batch_generator(1,'train_data.hdf',batch_size)
+generatorValid = batch_generator(0,'valid_data.hdf',batch_size)
 
-score = autoencoder.evaluate(inputs_valid, targets_valid, verbose=0)
-
-print('Validation Loss:', history.history['val_loss'][-1])
-
-model.save('cnnL2AE.h5')
+history=autoencoder.fit_generator(generatorTrain, samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch, verbose=1, validation_data=generatorValid, nb_val_samples=nb_val_samples, callbacks=[early_stopping, checkPointer])
 
 autoencoder.save('cnnL2AE.h5')
